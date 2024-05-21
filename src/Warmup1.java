c83370365510b4675767e1cc9b7879b57833bbb8
Azra <azramamic0@gmail.com>
2024-05-21 20:07:18 +0200

    Add next example...

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -14,8 +14,64 @@ public class Warmup1 {
 
     public boolean sleepIn(boolean weekday, boolean vacation) {
 
+        if (!weekday && !vacation){
+            return true;
+        } else if (weekday && !vacation) {
+            return false;
+        }
         return true;
     }
+    /*
+    We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is
+    smiling. We are in trouble if they are both smiling or if neither of them is smiling.
+    Return true if we are in trouble.
 
+   monkeyTrouble(true, true) → true
+   monkeyTrouble(false, false) → true
+   monkeyTrouble(true, false) → false
+   */
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+
+        if (aSmile && bSmile){
+            return true;
+        } else if (!aSmile && !bSmile) {
+            return true;
+        }
+        else return false;
+    }
+
+    /*
+    Given two int values, return their sum. Unless the two values are the same, then return
+    double their sum.
+
+    sumDouble(1, 2) → 3
+    sumDouble(3, 2) → 5
+    sumDouble(2, 2) → 8
+     */
+    public int sumDouble(int a, int b) {
+
+        int sum= a+b;
+
+        if (a==b){
+            return 2*sum;
+        }
+        else return sum;
+    }
+
+    /*
+    Given an int n, return the absolute difference between n and 21, except return double the
+    absolute difference if n is over 21.
+    diff21(19) → 2
+    diff21(10) → 11
+    diff21(21) → 0
+     */
+    public int diff21(int n) {
+
+        if (n <= 21) {
+            return 21 - n;
+        } else {
+            return (n - 21) * 2;
+        }
+    }
 
 }
