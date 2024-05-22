6d8589c3757b210dd75c7d344bf98bb365931e12
Azra <azramamic0@gmail.com>
2024-05-22 11:36:37 +0200

    Add next example...

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -73,5 +73,158 @@ public class Warmup1 {
             return (n - 21) * 2;
         }
     }
+    /*
+    We have a loud talking parrot. The "hour" parameter is the current hour time in the range
+    0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20.
+    Return true if we are in trouble.
+
+    parrotTrouble(true, 6) → true
+    parrotTrouble(true, 7) → false
+    parrotTrouble(false, 6) → false
+   */
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && hour <7 ){
+            return true;
+        } else if (talking && hour > 21) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+    makes10(9, 10) → true
+    makes10(9, 9) → false
+    makes10(1, 9) → true
+     */
+    public boolean makes10(int a, int b) {
+
+        if (a==10 || b==10){
+            return true;
+        }
+        else if (a+b == 10){
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num)
+     computes the absolute value of a number.
+
+     nearHundred(93) → true
+     nearHundred(90) → true
+     nearHundred(89) → false
+     */
+    public boolean nearHundred(int n) {
+
+        if (Math.abs(100-10) <= 10){
+            return true;
+        } else if (Math.abs(200-n) <= 10) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+
+    Given 2 int values, return true if one is negative and one is positive. Except if the
+    parameter "negative" is true, then return true only if both are negative.
+
+    posNeg(1, -1, false) → true
+    posNeg(-1, 1, false) → true
+    posNeg(-4, -5, true) → true
+     */
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (a>0 || b>0){
+            return true;
+        }
+        else if (negative){
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    Given a string, return a new string where "not " has been added to the front. However, if
+    the string already begins with "not", return the string unchanged. Note: use .equals() to
+    compare 2 strings.
+
+    notString("candy") → "not candy"
+    notString("x") → "not x"
+    notString("not bad") → "not bad"
+     */
+    public String notString(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        }
+        return "not " + str;
+    }
+
+    /*
+    Given a non-empty string and an int n, return a new string where the char at index n has
+    been removed. The value of n will be a valid index of a char in the original string
+    (i.e.n will be in the range 0..str.length()-1 inclusive).
+
+    missingChar("kitten", 1) → "ktten"
+    missingChar("kitten", 0) → "itten"
+    missingChar("kitten", 4) → "kittn"
+    */
+    public String missingChar(String str, int n) {
+
+        String front = str.substring(0, n);
+        String back = str.substring(n+1, str.length());
+
+        return front + back;
+    }
+
+    /*
+    Given a string, return a new string where the first and last chars have been exchanged.
+
+    frontBack("code") → "eodc"
+    frontBack("a") → "a"
+    frontBack("ab") → "ba"
+     */
+    public String frontBack(String str) {
+
+        if (str.length() <= 1) {
+            return str;
+        }
+        String mid = str.substring(1, str.length() - 1);
+        String first = str.substring(0);
+        String last = str.substring(str.length() - 1);
+
+        return last + mid + first;
+    }
+
+    /*
+    Given a string, we'll say that the front is the first 3 chars of the string. If the string
+    length is less than 3, the front is whatever is there. Return a new string which is 3 copies
+    of the front.
+
+    front3("Java") → "JavJavJav"
+    front3("Chocolate") → "ChoChoCho"
+    front3("abc") → "abcabcabc"
+    */
+    public String front3(String str) {
+
+        String front;
+        if (str.length() >= 3) {
+            front = str.substring(0, 3);
+        }
+        else {
+            front = str;
+        }
+        return front + front + front;
+    }
+    /*
+    Given two int values, return their sum. Unless the two values are the same, then return
+    double their sum.
+
+    sumDouble(1, 2) → 3
+    sumDouble(3, 2) → 5
+    sumDouble(2, 2) → 8
+    */
 
 }
